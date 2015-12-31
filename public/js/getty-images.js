2e2006ebaed288f3b25ec340e957a11d2b83f342
Chaz <cmtlum73@gmail.com>
2015-12-31 13:14:54 -1000

    got click to send ajax request and bring data back

---
+++ public/js/getty-images.js
@@ -0,0 +1,25 @@
+$( document ).ready( function () {
+
+  var myKey = 'fc4p6ewqjr6w9j8en4hwrjyh';
+
+  var searchButton = $( '<div>' );
+    searchButton
+      .addClass( 'searchButton' )
+      .text( 'Search for Pics!' )
+    ;
+  $( '.mainDiv' ).append( searchButton );
+
+  searchButton.click( function() {
+    var userInput = $( '.searchInput' ).val();
+
+    $.ajax( 'https://api.gettyimages.com/v3/search/images?phrase=' + userInput, {
+      headers : {
+        'Api-Key' : myKey
+      },
+      method : 'GET'
+      }).done( function( data ) {
+      console.log( data );
+    });
+
+  });
+});
